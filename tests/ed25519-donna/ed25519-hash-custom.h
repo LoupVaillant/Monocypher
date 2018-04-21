c92ec5dcfd53c9381902e82ebe453de6ab391ef3
Loup Vaillant <loup@loup-vaillant.fr>
2018-04-21 16:26:32 +0200

    Test vectors no longer depend on Monocypher
    Test vectors for EdDSA used to use Moncoypher's Blake2b hash. This
    didn't make much sense, and could even conceivably be construed as
    circular.  Blake2b vectors were properly generated, so it wasn't really
    circular, but that's still ugly.
    
    Now the test vectors only depend on Libsodium and ed25519-donna.

--- tests/ed25519-donna/ed25519-hash-custom.h
+++ tests/ed25519-donna/ed25519-hash-custom.h
@@ -1,38 +1,23 @@
-#include "monocypher.h"
+#include <sodium.h>
 
-
-#ifdef ED25519_SHA512
-    #include "rename_sha512.h"
-    #define HASH rename_sha512
-#else
-    #define HASH crypto_blake2b
-#endif
-#define COMBINE1(x, y) x ## y
-#define COMBINE2(x, y) COMBINE1(x, y)
-#define HASH_CTX    COMBINE2(HASH, _ctx)
-#define HASH_INIT   COMBINE2(HASH, _init)
-#define HASH_UPDATE COMBINE2(HASH, _update)
-#define HASH_FINAL  COMBINE2(HASH, _final)
-
-typedef struct {
-    HASH_CTX ctx;
-} ed25519_hash_context;
+typedef crypto_generichash_state ed25519_hash_context;
 
 void ed25519_hash_init(ed25519_hash_context *ctx)
 {
-    HASH_INIT(&(ctx->ctx));
+    crypto_generichash_init(ctx, 0, 0, 64);
 }
+
 void ed25519_hash_update(ed25519_hash_context *ctx, const uint8_t *in, size_t inlen)
 {
-    HASH_UPDATE(&(ctx->ctx), in, inlen);
+    crypto_generichash_update(ctx, in, inlen);
 }
+
 void ed25519_hash_final(ed25519_hash_context *ctx, uint8_t *hash)
 {
-    HASH_FINAL(&(ctx->ctx), hash);
+    crypto_generichash_final(ctx, hash, 64);
 }
+
 void ed25519_hash(uint8_t *hash, const uint8_t *in, size_t inlen)
 {
-    HASH(hash, in, inlen);
+    crypto_generichash(hash, 64, in, inlen, 0, 0);
 }
-
-
