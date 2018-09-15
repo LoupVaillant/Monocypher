450d571dea4df5cb231cb240315b802983c1e36f
Loup Vaillant <loup@loup-vaillant.fr>
2018-09-15 15:21:29 +0200

    Removed version numbers from coverage script

--- tests/coverage.sh
+++ tests/coverage.sh
@@ -4,5 +4,5 @@ set -e
 
 make clean
 make test CC="clang -std=c99" CFLAGS="-fprofile-instr-generate -fcoverage-mapping"
-llvm-profdata-3.8 merge default.profraw -o all.profdata
-llvm-cov-3.8 show -instr-profile=all.profdata "./test.out"
+llvm-profdata merge default.profraw -o all.profdata
+llvm-cov show -instr-profile=all.profdata "./test.out"
