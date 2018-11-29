07a9ff2aef41c80ebac2d24eff7015755b63b2e4
Loup Vaillant <loup@loup-vaillant.fr>
2018-11-29 23:05:23 +0100

    Test -DBLAKE2_NO_UNROLLING flag

--- tests/test.sh
+++ tests/test.sh
@@ -3,7 +3,7 @@
 set -e
 
 make clean;  make test
-make clean;  make test CFLAGS="-DED25519_SHA512 -O3"
+make clean;  make test CFLAGS="-DED25519_SHA512 -DBLAKE2_NO_UNROLLING -O3"
 make clean;  make test CC="clang -std=c99" CFLAGS="-g -fsanitize=address"
 make clean;  make test CC="clang -std=c99" CFLAGS="-g -fsanitize=memory"
 make clean;  make test CC="clang -std=c99" CFLAGS="-g -fsanitize=undefined"
