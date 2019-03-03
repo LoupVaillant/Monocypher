881a87395750da4ce167cf9523c85b905d713302
Loup Vaillant <loup@loup-vaillant.fr>
2019-03-03 22:56:29 +0100

    Added secure channel protocols (experimental)
    At long last, the NaCl family of crypto libraries is gaining direct
    support for secure channels.
    
    Up until now, the choices were basically invent our own protocol, or
    give up and use a TLS library, thus voiding the usability improvements
    of NaCl libraries.
    
    Now we have a solution.  It's still a bit experimental, it's not yet
    documented, but it's there.  And soon, we will finally be able to shift
    the cryptographic right answer for secure channels away from TLS, and
    towards the NaCl family.  Or perhaps just Monocypher, if for some reason
    Libsodium doesn't follow suit. :-)

--- src/monocypher.c
+++ src/monocypher.c
@@ -2165,3 +2165,213 @@ int crypto_unlock(u8       *plain_text,
     return crypto_unlock_aead(plain_text, key, nonce, mac, 0, 0,
                               cipher_text, text_size);
 }
+
+///////////////////////
+/// Secure channels ///
+///////////////////////
+static void copy32(u8 out[32], const u8 in[32]){FOR (i, 0, 32){out[i] = in[i];}}
+static void xor32 (u8 out[32], const u8 in[32]){FOR (i, 0, 32){out[i]^= in[i];}}
+
+static void kex_update_key(crypto_kex_ctx  *ctx,
+                           const u8 secret_key[32],
+                           const u8 public_key[32])
+{
+    static const u8 one[16] = {1};
+
+    // Extract
+    u8 shared_secret[32];
+    crypto_x25519(shared_secret, secret_key, public_key);
+    crypto_chacha20_H(shared_secret    , shared_secret    , zero);
+    crypto_chacha20_H(ctx->chaining_key, ctx->chaining_key, one );
+    xor32(ctx->chaining_key, shared_secret);
+
+    // Expand (directly from chaining key)
+    crypto_chacha_ctx chacha_ctx;
+    crypto_chacha20_init  (&chacha_ctx, ctx->chaining_key, one);
+    crypto_chacha20_stream(&chacha_ctx, ctx->derived_keys, 64);
+
+    // Clean up
+    WIPE_BUFFER(shared_secret);
+    WIPE_CTX(&chacha_ctx);
+}
+
+static void kex_auth(crypto_kex_ctx *ctx, u8 mac[16])
+{
+    crypto_poly1305(mac, ctx->transcript, ctx->transcript_size,
+                    ctx->derived_keys);
+}
+
+static int kex_verify(crypto_kex_ctx *ctx, const u8 mac[16])
+{
+    u8 real_mac[16];
+    kex_auth(ctx, real_mac);
+    int mismatch = crypto_verify16(real_mac, mac);
+    if (mismatch) {  WIPE_CTX(ctx); }
+    WIPE_BUFFER(real_mac);
+    return mismatch;
+}
+
+static void kex_send(crypto_kex_ctx *ctx, u8 msg[32], const u8 src[32])
+{
+    // Send message, encrypted if we have a key
+    copy32(msg, src);
+    xor32(msg, ctx->derived_keys + 32);
+    // Record sent message
+    copy32(ctx->transcript + ctx->transcript_size, msg);
+    ctx->transcript_size += 32;
+}
+
+static void kex_receive(crypto_kex_ctx *ctx, u8 dest[32], const u8 msg[32])
+{
+    // Record incoming message
+    copy32(ctx->transcript + ctx->transcript_size, msg);
+    ctx->transcript_size += 32;
+    // Receive message, decrypted it if we have a key
+    copy32(dest, msg);
+    xor32(dest, ctx->derived_keys + 32);
+}
+
+static void kex_init(crypto_kex_ctx *ctx,
+                     const u8        local_sk[32],
+                     const u8        local_pk[32])
+{
+    if (local_pk == 0) crypto_x25519_public_key(ctx->local_pk, local_sk);
+    else               copy32                  (ctx->local_pk, local_pk);
+    copy32(ctx->chaining_key     , zero    );
+    copy32(ctx->derived_keys + 32, zero    ); // first encryption key is zero
+    copy32(ctx->local_sk         , local_sk);
+    ctx->transcript_size = 0;
+}
+
+static void kex_seed(crypto_kex_ctx *ctx, u8 random_seed[32])
+{
+    copy32(ctx->local_ske        , random_seed);
+    crypto_wipe(random_seed, 32); // auto wipe seed to avoid reuse
+    crypto_x25519_public_key(ctx->local_pke, ctx->local_ske);
+}
+
+
+///////////
+/// XK1 ///
+///////////
+static const u8 xk1_ck0[32] = "Monokex XK1";
+void crypto_kex_xk1_init_client(crypto_kex_ctx *ctx,
+                                u8              random_seed[32],
+                                const u8        local_sk   [32],
+                                const u8        local_pk   [32],
+                                const u8        remote_pk  [32])
+{
+    kex_init   (ctx, local_sk, local_pk);
+    kex_seed   (ctx, random_seed);
+    kex_receive(ctx, ctx->remote_pk, remote_pk);
+    copy32(ctx->chaining_key, xk1_ck0);
+}
+
+void crypto_kex_xk1_init_server(crypto_kex_ctx *ctx,
+                                u8              random_seed[32],
+                                const u8        local_sk   [32],
+                                const u8        local_pk   [32])
+{
+    kex_init   (ctx, local_sk, local_pk);
+    kex_seed   (ctx, random_seed);
+    kex_receive(ctx, ctx->local_pk, ctx->local_pk);
+    copy32(ctx->chaining_key, xk1_ck0);
+}
+
+void crypto_kex_xk1_1(crypto_kex_ctx *ctx, u8 msg1[32])
+{
+    kex_send      (ctx, msg1           , ctx->local_pke );  // -> IE
+}
+
+void crypto_kex_xk1_2(crypto_kex_ctx *ctx, u8 msg2[48], const u8 msg1[32])
+{
+    kex_receive   (ctx, ctx->remote_pke, msg1           );  // -> IE
+    kex_send      (ctx, msg2           , ctx->local_pke );  // <- RE
+    kex_update_key(ctx, ctx->local_ske , ctx->remote_pke);  //    ee
+    kex_update_key(ctx, ctx->local_sk  , ctx->remote_pke);  //    es
+    kex_auth      (ctx, msg2 + 32);                         // auth
+}
+
+int crypto_kex_xk1_3(crypto_kex_ctx *ctx,
+                     u8              session_key[32],
+                     u8              msg3[48],
+                     const u8        msg2[48])
+{
+    kex_receive   (ctx, ctx->remote_pke, msg2           );  // <- RE
+    kex_update_key(ctx, ctx->local_ske , ctx->remote_pke);  //    ee
+    kex_update_key(ctx, ctx->local_ske , ctx->remote_pk );  //    es
+    if (kex_verify(ctx, msg2 + 32)) { return -1; }          // verify
+    kex_send      (ctx, msg3           , ctx->local_pk  );  // -> IS
+    kex_update_key(ctx, ctx->local_sk  , ctx->remote_pke);  //    se
+    kex_auth      (ctx, msg3 + 32);                         // auth
+    copy32(session_key, ctx->derived_keys + 32);
+    WIPE_CTX(ctx);
+    return 0;
+}
+
+int crypto_kex_xk1_4(crypto_kex_ctx *ctx,
+                     u8              session_key[32],
+                     u8              remote_pk[32],
+                     const u8        msg3[48])
+{
+    kex_receive   (ctx, ctx->remote_pk , msg3           );  // -> IS
+    kex_update_key(ctx, ctx->local_ske , ctx->remote_pk );  //    se
+    if (kex_verify(ctx, msg3 + 32)) { return -1; }          // verify
+    copy32(remote_pk  , ctx->remote_pk);
+    copy32(session_key, ctx->derived_keys + 32);
+    WIPE_CTX(ctx);
+    return 0;
+}
+
+/////////
+/// X ///
+/////////
+static const u8 x_ck0[32] = "Monokex X";
+void crypto_kex_x_init_client(crypto_kex_ctx *ctx,
+                              u8              random_seed[32],
+                              const u8        local_sk   [32],
+                              const u8        local_pk   [32],
+                              const u8        remote_pk  [32])
+{
+    kex_init   (ctx, local_sk, local_pk);
+    kex_seed   (ctx, random_seed);
+    kex_receive(ctx, ctx->remote_pk, remote_pk);
+    copy32(ctx->chaining_key, x_ck0);
+}
+
+void crypto_kex_x_init_server(crypto_kex_ctx *ctx,
+                              const u8        local_sk[32],
+                              const u8        local_pk[32])
+{
+    kex_init   (ctx, local_sk, local_pk);
+    kex_receive(ctx, ctx->local_pk, ctx->local_pk);
+    copy32(ctx->chaining_key, x_ck0);
+}
+
+void crypto_kex_x_1(crypto_kex_ctx *ctx, u8 session_key[32], u8 msg1[80])
+{
+    kex_send      (ctx, msg1           , ctx->local_pke );  // -> IE
+    kex_update_key(ctx, ctx->local_ske , ctx->remote_pk );  //    es
+    kex_send      (ctx, msg1 + 32      , ctx->local_pk  );  // -> IS
+    kex_update_key(ctx, ctx->local_sk  , ctx->remote_pk );  //    ss
+    kex_auth      (ctx, msg1 + 64);                         // auth
+    copy32(session_key, ctx->derived_keys + 32);
+    WIPE_CTX(ctx);
+}
+
+int crypto_kex_x_2(crypto_kex_ctx *ctx,
+                   u8              session_key[32],
+                   u8              remote_pk  [32],
+                   const u8        msg1       [80])
+{
+    kex_receive   (ctx, ctx->remote_pke, msg1           );  // -> IE
+    kex_update_key(ctx, ctx->local_sk  , ctx->remote_pke);  //    es
+    kex_receive   (ctx, ctx->remote_pk , msg1 + 32      );  // -> IS
+    kex_update_key(ctx, ctx->local_sk  , ctx->remote_pk );  //    ss
+    if (kex_verify(ctx, msg1 + 64)) { return -1; }          // verify
+    copy32(remote_pk  , ctx->remote_pk);
+    copy32(session_key, ctx->derived_keys + 32);
+    WIPE_CTX(ctx);
+    return 0;
+}
+
--- src/monocypher.h
+++ src/monocypher.h
@@ -64,6 +64,19 @@ typedef struct {
     uint8_t pk [32];
 } crypto_check_ctx;
 
+// Secure channels (Monokex)
+typedef struct {
+    uint8_t transcript [128];
+    uint8_t chaining_key[32];
+    uint8_t derived_keys[64];
+    uint8_t local_sk    [32];
+    uint8_t local_pk    [32];
+    uint8_t local_ske   [32];
+    uint8_t local_pke   [32];
+    uint8_t remote_pk   [32];
+    uint8_t remote_pke  [32];
+    size_t  transcript_size;
+} crypto_kex_ctx;
 
 ////////////////////////////
 /// High level interface ///
@@ -218,6 +231,51 @@ void crypto_check_init  (crypto_check_ctx *ctx,
 void crypto_check_update(crypto_check_ctx *ctx,
                          const uint8_t *message, size_t message_size);
 int crypto_check_final  (crypto_check_ctx *ctx);
+
+
+// Secure channel (interactive)
+// ----------------------------
+void crypto_kex_xk1_init_client(crypto_kex_ctx  *ctx,
+                                uint8_t       random_seed[32],
+                                const uint8_t local_sk   [32],
+                                const uint8_t local_pk   [32],
+                                const uint8_t remote_pk  [32]);
+void crypto_kex_xk1_init_server(crypto_kex_ctx  *ctx,
+                                uint8_t       random_seed[32],
+                                const uint8_t local_sk   [32],
+                                const uint8_t local_pk   [32]);
+
+void crypto_kex_xk1_1(crypto_kex_ctx *ctx,
+                      uint8_t         msg1[32]);
+void crypto_kex_xk1_2(crypto_kex_ctx *ctx,
+                      uint8_t         msg2[48],
+                      const uint8_t   msg1[32]);
+int crypto_kex_xk1_3(crypto_kex_ctx *ctx,
+                     uint8_t         session_key[32],
+                     uint8_t         msg3       [48],
+                     const uint8_t   msg2       [48]);
+int crypto_kex_xk1_4(crypto_kex_ctx *ctx,
+                     uint8_t         session_key[32],
+                     uint8_t         remote_pk  [32],
+                     const uint8_t   msg3       [48]);
+
+// Secure channel (one way)
+// ------------------------
+void crypto_kex_x_init_client(crypto_kex_ctx *ctx,
+                              uint8_t         random_seed[32],
+                              const uint8_t   local_sk   [32],
+                              const uint8_t   local_pk   [32],
+                              const uint8_t   remote_pk  [32]);
+void crypto_kex_x_init_server(crypto_kex_ctx *ctx,
+                              const uint8_t   local_sk   [32],
+                              const uint8_t   local_pk   [32]);
+void crypto_kex_x_1(crypto_kex_ctx *ctx,
+                    uint8_t         session_key[32],
+                    uint8_t         msg1       [80]);
+int crypto_kex_x_2(crypto_kex_ctx *ctx,
+                   uint8_t         session_key[32],
+                   uint8_t         remote_pk  [32],
+                   const uint8_t   msg1       [80]);
 
 
 ////////////////////////////
