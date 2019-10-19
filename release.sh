889e2eaa28c47f592bfdeb37c31aaeb1d8be23fc
Loup Vaillant <loup@loup-vaillant.fr>
2019-10-19 17:40:45 +0200

    Handle several "__git__" per line

--- release.sh
+++ release.sh
@@ -8,7 +8,7 @@ doc/man2html.sh
 rsync -avd --exclude-from=tarball_ignore ./ monocypher-$VERSION
 for file in `find monocypher-$VERSION -type f `
 do
-    sed -i "s/__git__/$VERSION/" $file
+    sed -i "s/__git__/$VERSION/g" $file
 done
 tar -czf monocypher-$VERSION.tar.gz monocypher-$VERSION
 rm -rf monocypher-$VERSION
