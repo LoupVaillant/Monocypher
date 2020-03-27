e795e7549ce86fc529439900fb5dbc2659ca9649
Loup Vaillant <loup@loup-vaillant.fr>
2020-03-27 18:16:32 +0100

    Test all possible key lengths for Blake2b

--- tests/gen/blake2b.c
+++ tests/gen/blake2b.c
@@ -55,7 +55,7 @@
 static void test(size_t size, size_t key_size, size_t hash_size)
 {
     RANDOM_INPUT(in  , 256);
-    RANDOM_INPUT(key , 32);
+    RANDOM_INPUT(key , 64);
     u8 hash[64];
 
     crypto_generichash(hash, hash_size, in, size, key, key_size);
@@ -70,7 +70,7 @@ int main(void)
 {
     SODIUM_INIT;
     FOR(size     , 0, 256) { test(size, 0       , 64       ); }
-    FOR(key_size , 0,  32) { test(128 , key_size, 64       ); }
+    FOR(key_size , 0,  64) { test(128 , key_size, 64       ); }
     FOR(hash_size, 1,  64) { test(128 , 0       , hash_size); }
     return 0;
 }
