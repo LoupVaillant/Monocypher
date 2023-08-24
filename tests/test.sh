b66694cc73d31d3d62b0f1a5f15b470c67117144
Loup Vaillant <loup@loup-vaillant.fr>
2023-08-24 19:02:14 +0200

    Run valgrind without march=native
    Valgrind doesn't always support all relevant instructions and crashes
    on some platforms, most notably github CI.

--- tests/test.sh
+++ tests/test.sh
@@ -59,7 +59,7 @@ make clean;  make test CFLAGS="-DBLAKE2_NO_UNROLLING -O3"
 make clean;  make test CC="clang -std=c99" CFLAGS="-g -fsanitize=address"
 make clean;  make test CC="clang -std=c99" CFLAGS="-g -fsanitize=memory"
 make clean;  make test CC="clang -std=c99" CFLAGS="-g -fsanitize=undefined"
-make clean;  make test.out;  valgrind ./test.out
+make clean;  make test.out CFLAGS="-g";  valgrind ./test.out
 
 echo
 echo "All sanitisers passed!"
